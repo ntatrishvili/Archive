06cb0b8011109a3730e62d708061aa4433a1b245
ntatrishvili <104984762+ntatrishvili@users.noreply.github.com>
2023-04-26 01:04:39 +0400

    Add files via upload

---
+++ RESTClIent/RESTClIent/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace RESTClIent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = string.Format("http://localhost:58180/Api/values?personalID={0}", textBox1.Text);
+
+            string response = Helper.Get(url);
+            var clientResponse = JsonConvert.DeserializeObject<client>(response);
+        }
+    }
+}
