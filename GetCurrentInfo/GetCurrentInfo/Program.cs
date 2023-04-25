170f3d50a5e3e7b1c0d37f32002f1505f2368434
ntatrishvili <104984762+ntatrishvili@users.noreply.github.com>
2023-04-26 00:56:36 +0400

    Add files via upload
    currentinfo & htmlpage

---
+++ GetCurrentInfo/GetCurrentInfo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetCurrentInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             public static string ConnectionString = @"Data Source=LAPTOP-QQDBCMF7\SQLEXPRESS;Initial Catalog=W3SCHOOL;Integrated Security = SSPI;";
+        
+            string commandText = "dbo.ARCHIVE_ORDERS";
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["nia"].ConnectionString))
+            {
+                using (var command = new SqlCommand())
+                {
+                    command.CommandText = commandText;
+                    command.Connection = conn;
+                    command.CommandType = CommandType.StoredProcedure;
+                    conn.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    
+    }
+}
