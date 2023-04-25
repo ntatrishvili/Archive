470e3705cd782da9ce3816a692403daab9035e74
ntatrishvili <104984762+ntatrishvili@users.noreply.github.com>
2023-04-26 01:09:00 +0400

    Add files via upload

---
+++ OrdersApp/OrdersApp/DbHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace OrdersApp
+{
+    public static class DbHelper
+    {
+        public static string ConnectionString = @"Data Source=LAPTOP-QQDBCMF7\SQLEXPRESS;Initial Catalog=W3SCHOOL;Integrated Security = SSPI;";
+        public static void SendArc()
+        {
+            string commandText = "dbo.ARCHIVE_ORDERS";
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["nia"].ConnectionString))
+            {
+                using (var command = new SqlCommand())
+                {
+                    command.CommandText = commandText;
+                    command.Connection = conn;
+                    command.CommandType = CommandType.StoredProcedure;
+                    conn.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ OrdersApp/OrdersApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OrdersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DbHelper.SendArc();
+        }
+    }
+}
