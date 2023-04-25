54ccd1a361e5262cd130d9bebec89512f6de73ff
ntatrishvili <104984762+ntatrishvili@users.noreply.github.com>
2023-04-26 00:58:24 +0400

    Add files via upload

---
+++ Leet1/Leet1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Leet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Hello World!");
+            int[] sum =TwoSum([2, 7, 14, 29],9);
+            string text = Console.ReadLine();
+            Console.WriteLine(text);
+            
+        }
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] returnValue = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i < j && (i + j) == target)
+                    {
+                        returnValue[0] = i;
+                        returnValue[1] = j;
+                    }
+                }
+            }
+            return returnValue;
+        }
+    }
+}
