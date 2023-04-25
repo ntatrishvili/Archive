2937876c0a5bf6b20d2fa98d2cb9f931e823e11e
ntatrishvili <104984762+ntatrishvili@users.noreply.github.com>
2023-04-26 00:59:34 +0400

    Add files via upload

---
+++ Rates/Rates/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rates.RateServiceReference;
+
+namespace Rates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (ServiceSoapClient client = new ServiceSoapClient())
+            {
+                CurrencyRate[] rates = client.GetCurrentRates("USD,EUR,GBP,GEL");
+                //int y = 50;
+                //foreach (var item in rates)
+                //{
+                //    Label label = new Label()
+                //    {
+                //        Text = item.Quantity.ToString() + " - " + item.Code.ToString() + "(" + item.Name.ToString() + ") - " + item.Rate + " (Diff:" + item.Diff + ")",
+                //        Location = new Point(20, y),
+                //        Width = 800
+                //    };
+                //    this.Controls.Add(label);
+                //    y += 50;
+                //}
+                dataGridView1.DataSource = Helper.GetRates(rates);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Rates/Rates/Helper.cs
@@ -0,0 +1,30 @@
+﻿using Rates.RateServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rates
+{
+    public static class Helper
+    {
+        public static DataTable GetRates(CurrencyRate[] rates)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Quantity");
+            table.Columns.Add("Rate");
+            table.Columns.Add("Name");
+            table.Columns.Add("Diff");
+            table.Columns.Add("Date");
+            table.Columns.Add("Code");
+            foreach (var item in rates)
+            {
+                table.Rows.Add(item.Quantity, item.Rate, item.Name, item.Diff, item.Date, item.Code);
+
+            }
+            return table;
+        }
+    }
+}
